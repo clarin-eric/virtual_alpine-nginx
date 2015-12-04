621871be9f9f73f71df45f5c9bf718b0f4c3c100
Sander M <S.N.Maijers@gmail.com>
2015-12-04 12:27:13 +0100

    ENHANCE: Let Packer provision the volumes, to further simplify the new handling of volumes.

--- provision.sh
+++ provision.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -ex
 
 apk --verbose --progress update --purge
-apk add 'nginx'
+apk add 'nginx' 'rsync'
+rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/etc/CLARIN_TLS/' '/vol/etc-CLARIN_TLS--infra/'
+rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/etc/nginx/' '/vol/etc-nginx--infra/'
+rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/srv/www/infra.clarin.eu/' '/vol/srv-www-infra.clarin.eu--infra/'
+apk del 'rsync'
