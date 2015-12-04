1ada11639b14d4dc33361f4147a4e8e8260ffdde
Sander M <S.N.Maijers@gmail.com>
2015-12-04 17:39:31 +0100

    * ENHANCE: Improve BUILD_REVISION usage in packer.json and provision.sh. * ENHANCE: Rewrite documentation to use Git revision as Docker tag.

--- provision.sh
+++ provision.sh
@@ -3,6 +3,6 @@
 apk --verbose --progress update --purge
 apk add 'nginx' 'rsync'
 rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/etc/CLARIN_TLS/' '/vol/etc-CLARIN_TLS--infra/'
-rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/etc/nginx/' '/vol/etc-nginx--infra/'
+rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/media/etc/nginx/' '/etc/nginx/'
 rsync --archive --cvs-exclude --human-readable --itemize-changes --delete '/srv/www/infra.clarin.eu/' '/vol/srv-www-infra.clarin.eu--infra/'
 apk del 'rsync'
